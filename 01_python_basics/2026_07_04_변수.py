4648464e2490d0b735beb9db11ed0ea6b11717f8
Jaemin Kim <jmkimtiger@gmail.com>
2026-07-05 00:15:19 +0900

    기초,반복문 연습

---
+++ 01_python_basics/2026_07_04_변수.py
@@ -0,0 +1,10 @@
+print("hi, semiconductor!")
+print(1+1)
+
+my_major = "chemical engineering"
+target_job = "semiconductor engineer"
+age = 24
+gpa = 3.7
+
+print(my_major, "student is studying to become a", target_job)
+print("My age is", age, "and my GPA is", gpa) 
---
+++ 01_python_basics/2026_07_04_조건문.py
@@ -0,0 +1,27 @@
+gpa = 3.7
+
+if gpa >= 3.5:
+    print("Eligible for scholarship")
+else:
+    print("Not eligible for scholarship")
+
+score = 20
+
+if score >=90:
+    print("A grade") 
+elif score >= 80:
+    print("B grade")
+elif score >= 70:
+    print("C grade")
+else:
+    print("Consider retaking the course")
+
+
+my_gpa = 3.69
+
+if my_gpa >= 4.0:
+    print("Excellent standing")
+elif my_gpa >= 3.5:
+    print("Good standing")
+else:
+    print("Needs improvement")  
